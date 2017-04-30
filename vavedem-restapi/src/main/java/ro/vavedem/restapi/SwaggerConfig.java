d5bfc534d06eeb15c862ddd2439fc66528d2960d
volosincu <bogdan.volosincu@riseup.net>
2017-04-30 19:58:36 +0000

    setare swagger

---
+++ vavedem-restapi/src/main/java/ro/vavedem/restapi/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package ro.vavedem.restapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by CoruptiaUcide
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ro.vavedem.restapi.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
